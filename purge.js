f311fec7981c386ca0e0069c7869c9491ada3113
zack <zack@zallen.net>
2024-07-10 16:40:44 -0400

    add purge.js

---
+++ purge.js
@@ -0,0 +1,32 @@
+import { readFileSync } from 'fs';
+
+const zone_id = 'fc96ebad666f68db3db62edc4847577c'; // openbible.io
+const invalidate_url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/purge_cache`;
+const max_per_request = 30;
+const to_invalidate = readFileSync(process.argv[2], 'utf8')
+	.split('\n')
+	.filter(Boolean);
+const token = process.env['CLOUDFLARE_PURGE_KEY'];
+
+const batch = [];
+while (to_invalidate.length > 0) {
+	batch.push(to_invalidate.pop());
+	if (batch.length == max_per_request) {
+		const body = JSON.stringify({ files: batch });
+		fetch(invalidate_url, {
+			method: 'POST',
+			headers: {
+				authorization: `Bearer ${token}`,
+				'content-type': 'application/json',
+			},
+			body,
+		})
+			.then(async res => {
+				if (!res.ok) {
+					console.log(body);
+					throw Error(await res.text());
+				}
+			});
+		batch.length = 0;
+	}
+}
